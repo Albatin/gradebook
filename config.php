197cce1c6c1561a071d18f32e61d4f18a0fe9165
Albatin Totaj <121321333+Albatin@users.noreply.github.com>
2024-05-30 10:55:13 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "dbgradebook.mysql.database.azure.com";
+$username = "albatin";
+$password = "albatin123";
 $dbname = "gradebook";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
